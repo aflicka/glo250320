b688bae1554ce8a9f27c3dc07a44046f336c772b
aflicka <62852536+aflicka@users.noreply.github.com>
2020-04-04 20:20:28 +0300

    dfgs
    fdgd

---
+++ glo250320/lesson06/script/script.js
@@ -0,0 +1,66 @@
+'use strict';
+
+let random = function() { 
+  return Math.floor(Math.random() * (100 - 1)) + 1
+}
+
+let isNumber = function(n){
+  return !isNaN(parseFloat(n)) && isFinite(n)
+};
+
+random();
+console.log(random());
+
+
+let start = function() {
+  let question = +prompt('загадайте число от 1 до 100');
+  console.log(question,typeof question)
+  if(question !== 0){
+    
+  }else{
+    alert('игра окончина')
+  }
+};
+
+start();
+
+
+
+
+
+// else if(question !== isNumber(question)) {
+//   alert('это не число');
+//   start()
+// }
+ // if(question !== null) {
+  //   question *=1;
+
+  //   if(question !==  isNumber(question)){
+      
+  //     if(question === random()){
+  //       alert('вы победили');
+  //     }else{
+  //       if(question < random()) {
+  //         alert('введите число больше!');
+  //         start()
+  //       }
+  //       else if(question > random()) {
+  //        alert('введите число меньше!');
+  //        start()
+  //       }
+  //       else if(question === null){
+  //         alert('игра окончена');
+  //       }
+  //     }
+
+  //   }
+  //   else{
+  //     alert('это не число');
+  //    start()
+  //   }
+  //   }
+  // else {
+  //   alert('игра окончена');
+  // };
+
+  // };
